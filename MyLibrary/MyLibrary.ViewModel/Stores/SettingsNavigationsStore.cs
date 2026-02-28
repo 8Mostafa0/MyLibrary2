3020e82f89841d62d9b75e1a1180499b867e70f1
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:26:25 +0330

    add SettingsNavigationsStore

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/SettingsNavigationsStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public class SettingNavigationStore
+    {
+        #region Dependencies
+        public event Action SettingViewModelChanged;
+
+        private ViewModelBase _currentSettingViewModel;
+
+
+
+
+        public ViewModelBase CurrentSettingViewModel
+        {
+            get => _currentSettingViewModel;
+            set
+            {
+                _currentSettingViewModel = value;
+                OnSettingViewModelChanged();
+            }
+        }
+        #endregion
+
+        #region Methods
+        public void OnSettingViewModelChanged()
+        {
+            SettingViewModelChanged?.Invoke();
+        }
+        public SettingNavigationStore() { }
+        #endregion
+    }
+}
