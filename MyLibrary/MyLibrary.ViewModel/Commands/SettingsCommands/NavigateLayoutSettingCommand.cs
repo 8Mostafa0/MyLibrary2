4a303bbea87a225b5ba1b2b367c7b6fde72da55d
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:34:54 +0330

    add NavigateLayoutSettingsCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/SettingsCommands/NavigateLayoutSettingCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace MyLibrary.ViewModel.Commands.SettingsCommands
+{
+    public class NavigateLayoutSettingCommand : CommandBase
+    {
+        #region Dependencies
+        private SettingNavigationStore _settingNavigationStore;
+        private MainLayoutSettingViewModel _mainLayoutSettingViewModel = new MainLayoutSettingViewModel();
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set setting view of setting navigation to main setting navigation
+        /// </summary>
+        /// <param name="settingNavigationStore"></param>
+        public NavigateLayoutSettingCommand(SettingNavigationStore settingNavigationStore)
+        {
+            _settingNavigationStore = settingNavigationStore;
+
+        }
+        #endregion
+
+        #region Execution
+        public override void Execute(object? parameter)
+        {
+            _settingNavigationStore.CurrentSettingViewModel = _mainLayoutSettingViewModel;
+        }
+        #endregion
+    }
+}
