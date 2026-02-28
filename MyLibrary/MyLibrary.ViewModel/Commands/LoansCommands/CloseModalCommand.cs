df78be144abe42d83482bab43687eeafd36b11a9
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:43:57 +0330

    add CloseModalCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/CloseModalCommand.cs
@@ -0,0 +1,33 @@
+﻿using MyLibrary.ViewModel.Stores;
+
+namespace MyLibrary.ViewModel.Commands.LoansCommands
+{
+    public class CloseModalCommand : CommandBase
+    {
+        #region Dependencies
+        private ModalNavigationStore _modalNavigationStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        ///  close modal by set current view model to null
+        /// </summary>
+        /// <param name="modalNavigationStore"></param>
+        public CloseModalCommand(ModalNavigationStore modalNavigationStore)
+        {
+            _modalNavigationStore = modalNavigationStore;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            _modalNavigationStore.Close();
+        }
+        #endregion
+    }
+}
