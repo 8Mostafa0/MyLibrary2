eb7305842b266665e426d719e1c021b957674bb7
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:36:24 +0330

    add SearchBookNameInReservedBookCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/SearchBookNameInReservedBookCommand.cs
@@ -0,0 +1,41 @@
+﻿namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
+{
+    public class SearchBookNameInReservedBookCommand : CommandBase
+    {
+        #region Dependencies
+        private ReservedBooksStore _reservedBooksStore;
+        private ReservedBooksViewModel _reservedBooksViewModel;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// search book name in reserved books list
+        /// </summary>
+        /// <param name="reservedBooksViewModel"></param>
+        /// <param name="reservedBooksStore"></param>
+        public SearchBookNameInReservedBookCommand(ReservedBooksViewModel reservedBooksViewModel, ReservedBooksStore reservedBooksStore)
+        {
+            _reservedBooksStore = reservedBooksStore;
+            _reservedBooksViewModel = reservedBooksViewModel;
+        }
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            if (!_reservedBooksViewModel.BookName.IsNullOrEmpty())
+            {
+                _reservedBooksStore.Clear();
+                string SearchSql = $"SELECT * FROM ReservedBooks WHERE EXISTS (SELECT 1 FROM Books WHERE Books.Name LIKE N'%{_reservedBooksViewModel.BookName}%' AND Books.Id = ReservedBooks.BookId )";
+                await _reservedBooksStore.GetReservedBooksAsync(SearchSql);
+            }
+        }
+        #endregion
+    }
+}
