638c76e403fbb75101db523ec6ad66e8d8e7f1b4
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:44:25 +0330

    add NavigationStore and Interface

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/Interfaces/INavigationStore.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary.ViewModel.ViewModels.Interfaces;
+using System;
+
+namespace MyLibrary.ViewModel.Stores.Interfaces
+{
+    public interface INavigationStore
+    {
+        IViewModelBase ContentScreen { get; set; }
+        INavigationBarViewModel MainContentViewModel { get; set; }
+        IStatusBarViewModel StatusBarViewModel { get; set; }
+
+        event Action ContentViewModelChanged;
+        event Action MainContentViewModelChanged;
+        event Action StatusBarViewModelChanged;
+    }
+}
