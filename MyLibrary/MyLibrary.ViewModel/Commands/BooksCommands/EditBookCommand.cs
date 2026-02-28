a59cf28c2ed2c4de079c4e17dd76859dbf65f552
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:48:32 +0330

    add EditBookCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/BooksCommands/EditBookCommand.cs
@@ -0,0 +1,89 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.BooksCommands
+{
+    public class EditBookCommand : CommandBase
+    {
+        #region Dependencies
+        private BooksStore _booksStore;
+        private BooksViewModel _booksViewModel;
+        private BooksRepository _booksRepository;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="booksViewModel"></param>
+        /// <param name="booksStore"></param>
+        /// <param name="booksRepository"></param>
+        public EditBookCommand(BooksViewModel booksViewModel, BooksStore booksStore, BooksRepository booksRepository)
+        {
+            _booksStore = booksStore;
+            _booksViewModel = booksViewModel;
+            _booksRepository = booksRepository;
+        }
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        ///  Checks To Validate Book Data First then Edite Selected Using BookStore Class.
+        /// </summary>
+        /// <param name="parameter">No Perameer Needed This Method Gets Its Data From BooksViewModel 
+        /// Data : Name,Publisher,Subject,PublicationDate</param>
+        public override async void Execute(object? parameter)
+        {
+            Book SelectedBook = _booksViewModel.SelectedBook;
+            if (SelectedBook == null)
+            {
+                MessageBox.Show("لطفا کتابی را برای ویرایش انتخاب کنید", "ویرایش کتاب");
+            }
+
+            if (_booksViewModel.Name.IsNullOrEmpty())
+            {
+                MessageBox.Show("لطفا نام کتاب را وارد کنید", "افزودن کتاب");
+                return;
+            }
+            if (_booksViewModel.Publisher.IsNullOrEmpty())
+            {
+                MessageBox.Show("لطفا منتشرکننده کتاب را وارد کنید", "افزودن کتاب");
+                return;
+            }
+            if (_booksViewModel.Subject.IsNullOrEmpty())
+            {
+                MessageBox.Show("لطفا نوع کتاب را وارد کنید", "افزودن کتاب");
+                return;
+            }
+            if (_booksViewModel.PublicationDate.IsNullOrEmpty())
+            {
+                MessageBox.Show("لطفا تاریخ انتشار کتاب را وارد کنید", "افزودن کتاب");
+                return;
+            }
+            int PublicationYear = 0;
+            if (!int.TryParse(_booksViewModel.PublicationDate, out PublicationYear))
+            {
+                MessageBox.Show("لطفا تاریخ انتشار کتاب را عدد وارد کنید", "افزودن کتاب");
+                _booksViewModel.PublicationDate = "";
+                return;
+            }
+
+
+
+            SelectedBook.Name = _booksViewModel.Name;
+            SelectedBook.Publisher = _booksViewModel.Publisher;
+            SelectedBook.Subject = _booksViewModel.Subject;
+            SelectedBook.PublicationDate = _booksViewModel.PublicationDate;
+            SelectedBook.Tier = _booksViewModel.Tier;
+            await _booksStore.EditBook(SelectedBook);
+        }
+        #endregion
+    }
+}
