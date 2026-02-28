a3594f193c919154ae3653fa3b0c21228dc53ee2
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:37:43 +0330

    add RemoveReservBookCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/RemoveReservBookCommand.cs
@@ -0,0 +1,45 @@
+﻿namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
+{
+    public class RemoveReservBookCommand : CommandBase
+    {
+        #region Dependencies
+        private ReservedBooksStore _reservedBooksStore;
+        private ReservedBooksViewModel _reservedBooksViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// validate selected reserv then ask for delete
+        /// </summary>
+        /// <param name="reservedBooksViewModel"></param>
+        /// <param name="reservedBooksStore"></param>
+        public RemoveReservBookCommand(ReservedBooksViewModel reservedBooksViewModel, ReservedBooksStore reservedBooksStore)
+        {
+            _reservedBooksStore = reservedBooksStore;
+            _reservedBooksViewModel = reservedBooksViewModel;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            if (_reservedBooksViewModel.SelectedReservedBook is null)
+            {
+                MessageBox.Show("لطفا نوبتی را برای حذف انتخاب کنید", "حذف نوبت");
+            }
+            else
+            {
+
+                var AskResult = MessageBox.Show("آیا از حذف این نوبت مطمن هستید؟", "حذف نوبت", MessageBoxButton.YesNo);
+                if (AskResult == MessageBoxResult.Yes)
+                {
+                    await _reservedBooksStore.DeleteReservBook(_reservedBooksViewModel.SelectedReservedBook.ToReservedBook());
+                }
+            }
+        }
+        #endregion
+    }
+}
