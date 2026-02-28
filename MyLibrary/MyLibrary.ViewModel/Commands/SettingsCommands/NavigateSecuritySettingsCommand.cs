1adc6702c9f56158e32c9722823c9f04049de4b3
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:33:43 +0330

    add NAvigateSecuritySettingsCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/SettingsCommands/NavigateSecuritySettingsCommand.cs
@@ -0,0 +1,35 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.SettingsViewModels;
+
+namespace MyLibrary.ViewModel.Commands.SettingsCommands
+{
+    public class NavigateSecuritySettingsCommand : CommandBase
+    {
+        #region Dependencies
+        private SettingNavigationStore _settingNavigationStore;
+        private SecuritySettingsViewModel _securitySettingsViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set current view of setting navigation to security (change password view) view
+        /// </summary>
+        /// <param name="settingNavigationStore"></param>
+        public NavigateSecuritySettingsCommand(SettingNavigationStore settingNavigationStore)
+        {
+            _settingNavigationStore = settingNavigationStore;
+            _securitySettingsViewModel = new SecuritySettingsViewModel(_settingNavigationStore);
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            _settingNavigationStore.CurrentSettingViewModel = _securitySettingsViewModel;
+        }
+        #endregion
+    }
+}
