4f899c0fe1570d31da1bbf2cad7e4aa5312c9c8c
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:10:52 +0330

    add Loan Model

---
+++ MyLibrary/MyLibrary.Model/Models/Loan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyLibrary.Model.Models
+{
+    public class Loan
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int BookId { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
