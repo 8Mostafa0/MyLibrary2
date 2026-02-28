ba8a9b257f1e47f0610d6659767677a14c3d4e56
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:44:26 +0330

    add SearchClientNameCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/SearchClientNameCommand.cs
@@ -0,0 +1,44 @@
+﻿using MyLibrary.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.ClientsCommands
+{
+    public class SearchClientNameCommand : CommandBase
+    {
+        #region Dependencies
+        private ClientsStore _clientsStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// search in clients database base on first name and last name
+        /// </summary>
+        /// <param name="clientsStore"></param>
+        public SearchClientNameCommand(ClientsStore clientsStore)
+        {
+            _clientsStore = clientsStore;
+        }
+        #endregion
+
+
+        #region Execution
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            string Name = _clientsStore.SearchClientName;
+            string SearchSql = $"SELECT * FROM Clients WHERE FirstName LIKE N'%{Name}%' OR LastName LIKE N'%{Name}%'";
+            await _clientsStore.GetOrderedClients(SearchSql);
+        }
+        #endregion
+    }
+}
