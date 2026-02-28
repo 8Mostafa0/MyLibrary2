1be35c7e2084d2d30c39f18b2debbdb47bc5a067
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:38:04 +0330

    add NavigateReservedBooksCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/NavigateReservedBooksCommand.cs
@@ -0,0 +1,60 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.ReservedBooksViewModels;
+
+namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
+{
+    public class NavigateReservedBooksCommand : CommandBase
+    {
+        #region Dependencies
+        private BooksStore _booksStore;
+        private ClientsStore _clientsStore;
+        private NavigationStore _navigationStore;
+        private ReservedBooksStore _reservedBooksStore;
+        private ModalNavigationStore _modalNavigationStore;
+        private ReservedBooksViewModel _reservedBooksViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="navigationStore"></param>
+        /// <param name="modalNavigationStore"></param>
+        /// <param name="reservedBooksStore"></param>
+        /// <param name="clientsStore"></param>
+        /// <param name="booksStore"></param>
+        /// <param name="loansRepository"></param>
+        /// <param name="clientsRepository"></param>
+        /// <param name="reservedBooksRepository"></param>
+        public NavigateReservedBooksCommand(
+            NavigationStore navigationStore,
+            ModalNavigationStore modalNavigationStore,
+            ReservedBooksStore reservedBooksStore,
+            ClientsStore clientsStore,
+            BooksStore booksStore,
+            LoanRepository loansRepository,
+            ClientsRepository clientsRepository,
+            ReservedBooksRepository reservedBooksRepository
+            )
+        {
+            _booksStore = booksStore;
+            _clientsStore = clientsStore;
+            _navigationStore = navigationStore;
+            _reservedBooksStore = reservedBooksStore;
+            _modalNavigationStore = modalNavigationStore;
+
+            _reservedBooksViewModel = ReservedBooksViewModel.LoadViewModel(_reservedBooksStore, _modalNavigationStore, _clientsStore, _booksStore, loansRepository, clientsRepository, reservedBooksRepository);
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.ContentScreen = _reservedBooksViewModel;
+        }
+        #endregion
+    }
+}
