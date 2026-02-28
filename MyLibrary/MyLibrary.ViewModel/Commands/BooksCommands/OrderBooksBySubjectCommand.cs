c0d68e1970eff11d559a4f3275b9b2dc0b3f213e
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:47:39 +0330

    add OrderBooksBySubjectCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/BooksCommands/OrderBooksBySubjectCommand.cs
@@ -0,0 +1,47 @@
+﻿namespace MyLibrary.ViewModel.Commands.BooksCommands
+{
+    public class OrderBooksBySubjectCommand : CommandBase
+    {
+        #region Dependencies
+        private BooksStore _booksStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// Order Book By Entered Subject
+        /// </summary>
+        /// <param name="booksStore"></param>
+        public OrderBooksBySubjectCommand(BooksStore booksStore)
+        {
+            _booksStore = booksStore;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// Order Book By Entered Subject
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override async void Execute(object? parameter)
+        {
+            if (_booksStore.SearchSubject < 0)
+            {
+                MessageBox.Show("لطفا ابتدا یک مورد برای ترتیب بندی انتخاب کنید", "ترتیب بندی");
+            }
+            string SubjectName = "";
+            switch (_booksStore.SearchSubject)
+            {
+                case 0: { SubjectName = "رمان"; break; }
+                case 1: { SubjectName = "قصه"; break; }
+                case 2: { SubjectName = "آموزشی"; break; }
+                case 3: { SubjectName = "معمایی"; break; }
+                case 4: { SubjectName = "خودشناسی"; break; }
+                case 5: { SubjectName = "شکرگذاری"; break; }
+            }
+            string SearchSql = $"SELECT * FROM Books WHERE Subject=N'{SubjectName}'";
+            await _booksStore.GetAllBooks(SearchSql);
+
+        }
+        #endregion
+    }
+}
