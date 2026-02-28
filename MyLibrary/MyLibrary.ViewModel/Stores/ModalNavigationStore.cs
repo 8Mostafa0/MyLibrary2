1d144bc91ab840a356cfff3691495d951efc4d2c
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:28:00 +0330

    add ModalNavigationStore

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/ModalNavigationStore.cs
@@ -0,0 +1,44 @@
+﻿using MyLibrary.ViewModel.ViewModels;
+using System;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public class ModalNavigationStore
+    {
+        #region Dependencies
+        private ViewModelBase _currentViewModel;
+        public bool IsModalOpen => CurrentViewModel != null;
+        public event Action CurrentViewModelChanged;
+        public ViewModelBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel?.Dispose();
+                _currentViewModel = value;
+                OnCurrentViewModelChange();
+            }
+        }
+
+
+        #endregion
+
+
+
+
+        #region Methods 
+
+        public void Close()
+        {
+            CurrentViewModel = null;
+            OnCurrentViewModelChange();
+        }
+
+        private void OnCurrentViewModelChange()
+        {
+            CurrentViewModelChanged?.Invoke();
+
+        }
+        #endregion
+    }
+}
