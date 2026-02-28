447dd7aaa56e7e2fb5418c4e4b1ac65b364cad4f
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:31:50 +0330

    add ViewModelBase with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/IViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+
+namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface IViewModelBase
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+        void Dispose();
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using MyLibrary.ViewModel.ViewModels.Interfaces;
+using System.ComponentModel;
+
+namespace MyLibrary.ViewModel.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged, IViewModelBase
+    {
+        #region Dependencies
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Constructor
+        protected void OnProperychanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Methods
+
+        public virtual void Dispose() { }
+        #endregion
+    }
+}
