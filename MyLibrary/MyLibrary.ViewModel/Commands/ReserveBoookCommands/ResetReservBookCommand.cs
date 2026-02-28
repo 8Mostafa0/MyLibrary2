2630385617ef2ffaf616b25e11a1a20d909ab853
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:37:23 +0330

    add ResetReservBookCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/ResetReservBookCommand.cs
@@ -0,0 +1,33 @@
+﻿using MyLibrary.ViewModel.Stores;
+
+namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
+{
+    public class ResetReservBookCommand : CommandBase
+    {
+        #region Dependencies
+        private ReservedBooksStore _reservedBooksStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// load all reserved book from database to reserved book store
+        /// </summary>
+        /// <param name="reservedBooksStore"></param>
+        public ResetReservBookCommand(ReservedBooksStore reservedBooksStore)
+        {
+            _reservedBooksStore = reservedBooksStore;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            await _reservedBooksStore.GetReservedBooksAsync();
+        }
+        #endregion
+    }
+}
