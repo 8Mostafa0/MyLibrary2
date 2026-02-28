b53d1308fa5b76ece3ada10db063bd8834d5440e
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:38:25 +0330

    add LoadReservedBooksCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/LoadReservedBooksCommand.cs
@@ -0,0 +1,40 @@
+﻿using MyLibrary.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
+{
+    public class LoadReservedBooksCommand : CommandBase
+    {
+        #region Dependencies
+        private ReservedBooksStore _reservedBooksStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// load all reservations from ReserveBooks table to reserved books store
+        /// </summary>
+        /// <param name="reservedBooksStore"></param>
+        public LoadReservedBooksCommand(ReservedBooksStore reservedBooksStore)
+        {
+            _reservedBooksStore = reservedBooksStore;
+        }
+        #endregion
+
+
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            await _reservedBooksStore.Load();
+        }
+        #endregion
+    }
+}
