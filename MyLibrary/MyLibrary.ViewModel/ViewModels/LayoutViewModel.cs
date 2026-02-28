e81419b2eb17e1e629c16a0fa9baf9507c09b0b8
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:16:16 +0330

    add LayoutViewModel

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/LayoutViewModel.cs
@@ -0,0 +1,42 @@
+﻿namespace MyLibrary.ViewModel.ViewModels
+{
+    public class LayoutViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private readonly NavigationStore _navigationStore;
+        public ViewModelBase contentViewModel => _navigationStore.ContentScreen;
+        public ViewModelBase MainContentViewModel => _navigationStore.MainContentViewModel;
+
+        public ViewModelBase StatusBarViewModel => _navigationStore.StatusBarViewModel;
+
+        #endregion
+
+        #region Constructor
+        public LayoutViewModel(NavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+            _navigationStore.ContentViewModelChanged += OnContentViewModelChanged;
+            _navigationStore.MainContentViewModelChanged += OnMainContentViewModelChanged;
+        }
+        #endregion
+
+        #region Methods
+
+
+        private void OnMainContentViewModelChanged()
+        {
+            OnProperychanged(nameof(MainContentViewModel));
+        }
+
+        private void OnStatusBarViewModelChanged()
+        {
+            OnProperychanged(nameof(StatusBarViewModel));
+        }
+
+        private void OnContentViewModelChanged()
+        {
+            OnProperychanged(nameof(contentViewModel));
+        }
+        #endregion
+    }
+}
