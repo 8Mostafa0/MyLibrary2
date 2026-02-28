5a622142c74c2d695438aaeea5efb79b6b5e4d8b
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:31:29 +0330

    add LoginModalCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoginModalCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLibrary.ViewModel.Commands
+{
+    public class LoginModalCommand : CommandBase
+    {
+        #region Dependencies
+        private LoginViewModel _loginViewModel;
+        private ModalNavigationStore _modalNavigationStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set current view of modal navigation to login view
+        /// </summary>
+        /// <param name="modalNavigationStore"></param>
+        /// <param name="loanRepository"></param>
+        /// <param name="dbContextFactory"></param>
+        /// <param name="settingsStores"></param>
+        public LoginModalCommand(ModalNavigationStore modalNavigationStore, LoanRepository loanRepository, DbContextFactory dbContextFactory, SettingsStore settingsStore)
+        {
+            _modalNavigationStore = modalNavigationStore;
+            _loginViewModel = new LoginViewModel(_modalNavigationStore, loanRepository, dbContextFactory, settingsStore);
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override void Execute(object? parameter)
+        {
+            _modalNavigationStore.CurrentViewModel = _loginViewModel;
+        }
+        #endregion
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/Commands/NavigateHomeCommand.cs
@@ -0,0 +1,46 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+
+namespace MyLibrary.ViewModel.Commands
+{
+    public class NavigateHomeScreenCommand : CommandBase
+    {
+        #region Dependencies
+        private LoansStore _loansStore;
+        private BooksStore _booksStore;
+        private ClientsStore _clientsStore;
+        private HomeViewModel _homeViewModel;
+        private readonly NavigationStore _navigationStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set current view of main navigation to home view 
+        /// </summary>
+        /// <param name="navigationStore"></param>
+        /// <param name="loansStore"></param>
+        /// <param name="clientsStore"></param>
+        /// <param name="booksStore"></param>
+        public NavigateHomeScreenCommand(NavigationStore navigationStore, LoansStore loansStore, ClientsStore clientsStore, BooksStore booksStore)
+        {
+            _navigationStore = navigationStore;
+            _clientsStore = clientsStore;
+            _booksStore = booksStore;
+            _loansStore = loansStore;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            await _clientsStore.Load();
+            await _booksStore.Load();
+            await _loansStore.Load();
+            _navigationStore.ContentScreen = new HomeViewModel(_clientsStore, _booksStore, _loansStore);
+        }
+        #endregion
+    }
+}
