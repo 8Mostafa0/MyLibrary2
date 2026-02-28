75fdc9f10674f953bf15489b4f685acc456f3419
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:09:12 +0330

    add Client Model

---
+++ MyLibrary/MyLibrary.Model/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyLibrary.Model.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Tier { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
