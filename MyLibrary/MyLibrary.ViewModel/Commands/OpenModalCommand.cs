f2bb334ae692f1eee3b653d6c11d9bf4cee4b0d1
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:30:07 +0330

    add OpenModalCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/OpenModalCommand.cs
@@ -0,0 +1,41 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands
+{
+    public class OpenModalCommand : CommandBase
+    {
+        #region Dependencies
+        private Func<ViewModelBase> _createViewModel;
+        private ModalNavigationStore _modalNavigationStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set vurrent view of modal navigation to Func of the inputed view model
+        /// </summary>
+        /// <param name="modalNavigationStore"></param>
+        /// <param name="createViewModel"></param>
+        public OpenModalCommand(ModalNavigationStore modalNavigationStore, Func<ViewModelBase> createViewModel)
+        {
+            _modalNavigationStore = modalNavigationStore;
+            _createViewModel = createViewModel;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            _modalNavigationStore.CurrentViewModel = _createViewModel();
+        }
+        #endregion
+    }
+}
