763111f6af92e6db2b53229cd5f35958f39a290d
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:08:37 +0330

    add Book Model

---
+++ MyLibrary/MyLibrary.Model/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyLibrary.Model.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Publisher { get; set; }
+        public string Subject { get; set; }
+        public string PublicationDate { get; set; }
+        public int Tier { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
