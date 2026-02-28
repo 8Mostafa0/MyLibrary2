4167a56efb915adde8b9bc67bd19310ad22ad413
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:43:41 +0330

    add LoadLoansCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/LoadLoansCommand.cs
@@ -0,0 +1,33 @@
+﻿using MyLibrary.ViewModel.Stores;
+
+namespace MyLibrary.ViewModel.Commands.LoansCommands
+{
+    public class LoadLoansCommand : CommandBase
+    {
+        #region Dependencies
+        private LoansStore _loansStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// load all loans from database to loans store
+        /// </summary>
+        /// <param name="loansStore"></param>
+        public LoadLoansCommand(LoansStore loansStore)
+        {
+            _loansStore = loansStore;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            await _loansStore.GetAllLoans();
+        }
+        #endregion
+    }
+}
