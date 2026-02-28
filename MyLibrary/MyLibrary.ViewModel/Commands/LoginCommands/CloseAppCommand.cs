d3561f40caddbdcc3fdfa780e63a8c956d3bb026
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:40:38 +0330

    add CloseAppCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoginCommands/CloseAppCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MyLibrary.ViewModel.Commands.LoginCommands
+{
+    public class CloseAppCommand : CommandBase
+    {
+        #region Contructor
+        /// <summary>
+        /// </summary>
+        public CloseAppCommand() { }
+        #endregion
+
+
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            var AskMessage = MessageBox.Show("میخواهید برنامه را ببندید؟", "خروج", MessageBoxButton.YesNo);
+            if (AskMessage == MessageBoxResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+        }
+        #endregion
+    }
+}
