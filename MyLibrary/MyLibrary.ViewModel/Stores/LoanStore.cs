3ad6ece06c283b20fd5526e911558376ef9423d7
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:28:27 +0330

    add LoanStore

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/LoanStore.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public class LoansStore
+    {
+
+        #region Dependencies
+        public IEnumerable<LoanViewModel> Loans => _loans;
+        private List<LoanViewModel> _loans;
+        private LoanRepository _loanRepository;
+        private ClientsStore _clientsStore;
+        private BooksStore _booksStore;
+
+        public Action LoansUpdated;
+        public Action<Loan> LoanIsAdded;
+        public Action<Loan> LoanIsReturned;
+        public Action<Loan> LoanIsUpdated;
+        public Lazy<Task> _initilizeLazy;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// 
+        /// </summary>
+        public LoansStore()
+        {
+            _loans = [];
+            _initilizeLazy = new Lazy<Task>(Initialize);
+            _loanRepository = new LoanRepository();
+            _clientsStore = new ClientsStore();
+            new LoadClientsCommand(_clientsStore).Execute(null);
+            _booksStore = new BooksStore();
+            new LoadBooksCommand(_booksStore).Execute(null);
+        }
+        #endregion
+
+
+        #region Methods
+
+        /// <summary>
+        /// fill loans list for first time and next time use in memory values
+        /// </summary>
+        /// <returns></returns>
+        public async Task Load()
+        {
+            await _initilizeLazy.Value;
+        }
+
+        /// <summary>
+        /// call for setloanreturned method of database and invoke returned loand event
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns></returns>
+        public async Task LoanReturned(Loan loan)
+        {
+            await _loanRepository.SetLoanReturned(loan);
+            LoanIsReturned?.Invoke(loan);
+        }
+
+        /// <summary>
+        /// call for update loan method of database and invoke for update loan event
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns></returns>
+        public async Task LoanUpdated(Loan loan)
+        {
+            await _loanRepository.UpdateLoanAtDb(loan);
+            LoanIsUpdated?.Invoke(loan);
+        }
+        /// <summary>
+        /// call for add loan method of database and invoke for add new loan event
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns></returns>
+        public async Task AddLoan(Loan loan)
+        {
+            await _loanRepository.AddNewLoanToDb(loan);
+            LoanIsAdded?.Invoke(loan);
+        }
+
+        /// <summary>
+        /// clear all lons set in memory and fill it with new loans geted from database
+        /// </summary>
+        /// <param name="customSql"></param>
+        /// <returns></returns>
+        public async Task GetAllLoans(string customSql = "")
+        {
+            IEnumerable<Loan> loans = await _loanRepository.GetAllLoans(customSql);
+            _loans.Clear();
+            foreach (Loan loan in loans)
+            {
+                LoanViewModel loanViewModel = new(loan, _clientsStore, _booksStore);
+                _loans.Add(loanViewModel);
+            }
+            LoansUpdated?.Invoke();
+        }
+
+        /// <summary>
+        /// get loans list from database and fill for first time
+        /// </summary>
+        /// <returns></returns>
+        private async Task Initialize()
+        {
+            IEnumerable<Loan> loans = await _loanRepository.GetAllLoans();
+            _loans.Clear();
+            foreach (Loan loan in loans)
+            {
+                LoanViewModel loanViewModel = new(loan, _clientsStore, _booksStore);
+                _loans.Add(loanViewModel);
+            }
+            LoansUpdated?.Invoke();
+        }
+
+        /// <summary>
+        /// call for loan update method of database  and invvoke loan update event
+        /// </summary>
+        /// <param name="loan"></param>
+        /// <returns></returns>
+        public async Task UpdateLoan(Loan loan)
+        {
+            await _loanRepository.UpdateLoanAtDb(loan);
+            LoanIsUpdated?.Invoke(loan);
+        }
+        #endregion
+    }
+}
