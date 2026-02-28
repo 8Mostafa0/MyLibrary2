9628e22bde461f0472f6479b91db1bc6d5d26a9d
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:43:25 +0330

    add NavigateLoansCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/NavigateLoansCommand.cs
@@ -0,0 +1,45 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.LoanViewModels;
+
+namespace MyLibrary.ViewModel.Commands.LoansCommands
+{
+    public class NavigateLoansCommand : CommandBase
+    {
+        #region Dependencies
+        private LoansViewModel _loansViewModel;
+        private NavigationStore _navigationStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// set content of navigate to loans view model
+        /// </summary>
+        /// <param name="navigationStore"></param>
+        /// <param name="modalNavigationStore"></param>
+        /// <param name="loansStore"></param>
+        /// <param name="clientsStore"></param>
+        /// <param name="booksStore"></param>
+        /// <param name="loanRepository"></param>
+        /// <param name="settingsStore"></param>
+        /// <param name="booksRepository"></param>
+        /// <param name="reservedBooksRepository"></param>
+        public NavigateLoansCommand(NavigationStore navigationStore, ModalNavigationStore modalNavigationStore, LoansStore loansStore, ClientsStore clientsStore, BooksStore booksStore, LoanRepository loanRepository, SettingsStore settingsStore, BooksRepository booksRepository, ReservedBooksRepository reservedBooksRepository)
+        {
+            _navigationStore = navigationStore;
+            _loansViewModel = LoansViewModel.LoadViewModel(modalNavigationStore, loansStore, clientsStore, booksStore, loanRepository, settingsStore, booksRepository, reservedBooksRepository);
+        }
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.ContentScreen = _loansViewModel;
+        }
+        #endregion
+    }
+}
