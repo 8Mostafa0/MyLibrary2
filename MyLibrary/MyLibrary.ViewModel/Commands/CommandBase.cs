0422b9d552182109698e23f2bd45197e8242d108
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:31:52 +0330

    add CommandBase

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/CommandBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MyLibrary.ViewModel.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        #region Properties
+        public event EventHandler? CanExecuteChanged;
+        #endregion
+
+        #region Methods
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object? parameter);
+
+
+        protected void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+        #endregion
+    }
+}
