4771c74091bf4a547267902444643101de3a2f71
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:34:20 +0330

    add NavigateLoanSettingsCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/SettingsCommands/NavigateLoanSettingsCommand.cs
@@ -0,0 +1,31 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.SettingsViewModels;
+
+namespace MyLibrary.ViewModel.Commands.SettingsCommands
+{
+    public class NavigateLoanSettingsCommand : CommandBase
+    {
+        #region Dependencies
+        private SettingNavigationStore _navigationStore;
+        private LoanSettingsViewModel _loanSettingsViewModel = new LoanSettingsViewModel();
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set current view model of setting navigation to loan loan settings
+        /// </summary>
+        /// <param name="navigationStore"></param>
+        public NavigateLoanSettingsCommand(SettingNavigationStore navigationStore)
+        {
+            _navigationStore = navigationStore;
+        }
+        #endregion
+
+        #region Execution
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentSettingViewModel = _loanSettingsViewModel;
+        }
+        #endregion
+    }
+}
