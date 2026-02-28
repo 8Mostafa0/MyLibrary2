98e054928c9cf8d188b539c82901114d337e17fe
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:33:51 +0330

    add MainViewModel with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/IMainViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface IMainViewModel
+    {
+        IViewModelBase CurrentModalView { get; }
+        IViewModelBase CurrentViewModel { get; }
+        bool IsModalOpen { get; }
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MyLibrary.ViewModel.ViewModels.Interfaces;
+
+namespace MyLibrary.ViewModel.ViewModels
+{
+    public class MainViewModel : ViewModelBase, IMainViewModel
+    {
+        #region Dependencies
+        private readonly IViewModelBase _layoutViewModel;
+        private IModalNavigationStore _modalNavigationStore;
+        public IViewModelBase CurrentViewModel => _layoutViewModel;
+        public IViewModelBase CurrentModalView => _modalNavigationStore.CurrentViewModel;
+
+        public bool IsModalOpen => _modalNavigationStore.IsModalOpen;
+        #endregion
+        #region Constructor
+        public MainViewModel(ILayoutViewModel layoutViewModel, IModalNavigationStore modalNavigationStore, ILoanRepository loanRepository, IDbContextFactory dbContextFactory, ISettingsStore settingsStore)
+        {
+            _layoutViewModel = layoutViewModel;
+            _modalNavigationStore = modalNavigationStore;
+            _modalNavigationStore.CurrentViewModelChanged += OnModalChanged;
+            new LoginModalCommand(_modalNavigationStore, loanRepository, dbContextFactory, settingsStore).Execute(null);
+
+        }
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// get called each tim change value of modal navigation event trigred
+        /// </summary>
+        private void OnModalChanged()
+        {
+            OnProperychanged(nameof(CurrentModalView));
+            OnProperychanged(nameof(IsModalOpen));
+        }
+
+        #endregion
+    }
+}
