2f842a5810a3ed21b256b9e7fe37404723d07085
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:45:30 +0330

    add LoadClientsCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/LoadClientsCommand.cs
@@ -0,0 +1,36 @@
+﻿using MyLibrary.ViewModel.Stores;
+
+namespace MyLibrary.ViewModel.Commands.ClientsCommands
+{
+    public class LoadClientsCommand : CommandBase
+    {
+        #region Dependencies
+        private readonly ClientsStore _clientsStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// load clients from database to clients store
+        /// </summary>
+        /// <param name="clientsStore"></param>
+        public LoadClientsCommand(ClientsStore clientsStore)
+        {
+            _clientsStore = clientsStore;
+        }
+
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no parameters needed</param>
+        public override async void Execute(object? parameter)
+        {
+            await _clientsStore.GetOrderedClients();
+        }
+        #endregion
+    }
+}
