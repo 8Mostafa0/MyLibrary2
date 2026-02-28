f0f2167ac40351e705d705db71fe2ecc34e0682e
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:46:50 +0330

    Add SearchBookNameCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/BooksCommands/SearchBookNameCommand.cs
@@ -0,0 +1,40 @@
+﻿using MyLibrary.ViewModel.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.BooksCommands
+{
+    public class SearchBookNameCommand : CommandBase
+    {
+        #region Dependencies
+        private BooksStore _booksStore;
+        #endregion
+
+
+        #region Contructor
+        /// <summary>
+        /// Search Book Using Name From BookStore.
+        /// </summary>
+        /// <param name="booksStore"></param>
+        public SearchBookNameCommand(BooksStore booksStore)
+        {
+            _booksStore = booksStore;
+        }
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            string SearchSql = $"SELECT * FROM Books WHERE Name LIKE N'%{_booksStore.SearchBookName}%'";
+            await _booksStore.GetAllBooks(SearchSql);
+        }
+        #endregion
+    }
