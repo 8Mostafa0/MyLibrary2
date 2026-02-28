f0485586a80f4f883f7a2854285967ac03e60c49
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:49:32 +0330

    add HomeViewModel with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/IHomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface IHomeViewModel : IViewModelBase
+    {
+        string BooksCount { get; set; }
+        string ClientsCount { get; set; }
+        string DilayedLoanCount { get; set; }
+        string LoansCount { get; set; }
+        string ShowBooksCount { get; }
+        string ShowClientsCount { get; set; }
+        string ShowDilayedLoanCount { get; set; }
+        string ShowLoansCount { get; set; }
+    }
+}
