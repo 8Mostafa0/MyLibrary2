a88b025899386c3d17aab04ffd48faf5b3c36d04
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:18:42 +0330

    add SecuritySettingsViewModel

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/SettingsViewModels/SecuritySettingsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using System.Windows.Input;
+
+namespace MyLibrary.ViewModel.ViewModels.SettingsViewModels
+{
+    public class SecuritySettingsViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                if (!value.IsNullOrEmpty() || value.Count() >= 5)
+                {
+                    _password = value;
+                    OnProperychanged(nameof(Password));
+                }
+                else
+                {
+                    _password = null;
+                    MessageBox.Show("لطفا رمز عبور را بیشتر از 5 حرف وارد کنید", "رمز عبور");
+                }
+            }
+        }
+        private SettingsStore _settingsStore;
+        private SettingNavigationStore _settingNavigationStore;
+        #endregion
+
+        #region Commands
+        public ICommand ChangeLoginPasswordCommand { get; }
+        #endregion
+
+        #region Contructor
+        public SecuritySettingsViewModel(SettingNavigationStore settingNavigationStore)
+        {
+            _settingsStore = new SettingsStore();
+            _settingNavigationStore = settingNavigationStore;
+            ChangeLoginPasswordCommand = new ChangeLoginPasswordCommand(this, _settingNavigationStore, _settingsStore);
+        }
+
+        #endregion
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/SettingsViewModels/SettingsViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows.Input;
+
+namespace MyLibrary.ViewModel.ViewModels.SettingsViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private SettingNavigationStore _settingNavigationStore;
+
+        public ViewModelBase CurrentSettingViewModel => _settingNavigationStore.CurrentSettingViewModel;
+
+        #endregion
+
+        #region Commands
+        public ICommand NavigateLayoutSettingCommand { get; }
+        public ICommand NavigateLoanSettingsCommand { get; }
+        public ICommand NavigateSecuritySettingsCommand { get; }
+        #endregion
+
+        #region Contructor
+        public SettingsViewModel(SettingNavigationStore settingNavigationStore)
+        {
+            _settingNavigationStore = settingNavigationStore;
+            _settingNavigationStore.SettingViewModelChanged += OnSettingViewModelChanged;
+            NavigateLayoutSettingCommand = new NavigateLayoutSettingCommand(_settingNavigationStore);
+            NavigateLoanSettingsCommand = new NavigateLoanSettingsCommand(_settingNavigationStore);
+            NavigateSecuritySettingsCommand = new NavigateSecuritySettingsCommand(_settingNavigationStore);
+        }
+
+        #endregion
+
+        #region Methods
+        private void OnSettingViewModelChanged()
+        {
+            OnProperychanged(nameof(CurrentSettingViewModel));
+        }
+        #endregion
+
+    }
+}
