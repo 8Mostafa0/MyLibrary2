8e16a0d46ea35a3561b44009d663e63c321afacb
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:46:15 +0330

    add NavigationBarViewModel with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/INavigationBarViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface INavigationBarViewModel
+    {
+        INavigateClientScreenCommand ClientsCreenCommand { get; }
+        ICloseAppCommand CloseAppCommand { get; }
+        ICommand DatabaseCommand { get; }
+        INavigateBooksCommand NavigateBooksCommand { get; }
+        INavigateHomeScreenCommand NavigateHomeCommand { get; }
+        INavigateLoansCommand NavigateLoansCommand { get; }
+        INavigateReservedBooksCommand NavigateReservedBooksCommand { get; }
+        INavigateToSettingsCommand NavigateToSettingsCommand { get; }
+        IOpenModalCommand OpenModalCommand { get; }
+        virtual void Dispose() { }
+    }
+}
