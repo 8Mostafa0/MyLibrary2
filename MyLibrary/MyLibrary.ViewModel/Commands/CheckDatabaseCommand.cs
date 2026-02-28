e8cd940d163c4ccf88828d9b5edc489a0fc57c21
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:32:19 +0330

    add CheckDatabaseCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/CheckDatabaseCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace MyLibrary.ViewModel.Commands
+{
+    public class CheckDatabaseCommand : CommandBase
+    {
+        #region Dependencies
+        private LoanRepository _loansRepository;
+        private DbContextFactory _dbContextFactory;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// check ans validate database and tables
+        /// </summary>
+        public CheckDatabaseCommand(LoanRepository loanRepository, DbContextFactory dbContextFactory)
+        {
+            _loansRepository = loanRepository;
+            _dbContextFactory = dbContextFactory;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter"></param>
+        public override async void Execute(object? parameter)
+        {
+            await _dbContextFactory.CheckDatabaseExistsAsync();
+        }
+        #endregion
+    }
+}
