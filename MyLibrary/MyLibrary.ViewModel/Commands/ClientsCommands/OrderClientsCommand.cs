b7bb39ec7fe3baa946ad2b1ce542d31afdf895b4
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:44:59 +0330

    add OrderClientsCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/OrderClientsCommand.cs
@@ -0,0 +1,39 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+
+namespace MyLibrary.ViewModel.Commands.ClientsCommands
+{
+    public class OrderClientsCommand : CommandBase
+    {
+        #region Dependencies
+        private readonly ClientsStore _clitentsStore;
+        private readonly ClientsViewModel _clientsViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// order clients based on tier
+        /// </summary>
+        /// <param name="clientsStore"></param>
+        /// <param name="clientsViewModel"></param>
+        public OrderClientsCommand(ClientsStore clientsStore, ClientsViewModel clientsViewModel)
+        {
+            _clitentsStore = clientsStore;
+            _clientsViewModel = clientsViewModel;
+        }
+        #endregion
+
+        #region Execution
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            string customSql = $"SELECT * FROM Clients WHERE Tier = '{_clientsViewModel.SortOrder}'";
+            await _clitentsStore.GetOrderedClients(customSql);
+        }
+        #endregion
+    }
+}
