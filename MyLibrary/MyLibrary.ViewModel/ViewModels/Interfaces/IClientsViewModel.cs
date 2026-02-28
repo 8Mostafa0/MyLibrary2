7b1e5788cf40ec2380640a0299ad54673de7cbfd
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:51:43 +0330

    add ClientsViewModel with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/IClientsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface IClientsViewModel : IViewModelBase
+    {
+        IAddNewClientCommand AddNewClientCommand { get; }
+        IEnumerable<IClient> Clients { get; }
+        IDeleteClientCommand DeleteClientCommand { get; }
+        IEditClientCommand EditClientCommand { get; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        ILoadClientsCommand LoadClientsCommand { get; }
+        IOrderClientsCommand OrderClientsCommand { get; }
+        IReloadClientsCommand ReloadClientsCommand { get; }
+        IClient SelectedClient { get; set; }
+        string SortOrder { get; set; }
+        int Tier { get; set; }
+
+        static abstract IClientsViewModel LoadViewModel(IClientsStore clientStore, ILoanRepository loanRepository, IReservedBooksRepository reservedBooksRepository);
+        void UpdateClients();
+    }
+}
