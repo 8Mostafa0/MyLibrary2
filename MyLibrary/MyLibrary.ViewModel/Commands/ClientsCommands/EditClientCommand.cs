39958e4d07eb03e7a56d7fd9401d1c3063e51f94
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:45:49 +0330

    add EditClientCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/EditClientCommand.cs
@@ -0,0 +1,62 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.ClientsCommands
+{
+    public class EditClientCommand : CommandBase
+    {
+        #region Dependencies
+        private ClientsStore _clientsStore;
+        private ClientsViewModel _clientsViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        ///  validate selected client then input values the edite client using clients store
+        /// </summary>
+        /// <param name="clientsViewModel"></param>
+        /// <param name="clientsStore"></param>
+        public EditClientCommand(ClientsViewModel clientsViewModel, ClientsStore clientsStore)
+        {
+            _clientsViewModel = clientsViewModel;
+            _clientsStore = clientsStore;
+        }
+        #endregion
+
+
+        #region Execution
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="parameter">no parametes needed</param>
+        public override async void Execute(object? parameter)
+        {
+            Client client = _clientsViewModel.SelectedClient;
+            if (client == null)
+            {
+                MessageBox.Show("لطفا کاربری را برای ویرایش انتخاب کنید", "ویرایش کاربر");
+                return;
+            }
+            if (string.IsNullOrEmpty(_clientsViewModel.FirstName))
+            {
+                MessageBox.Show("لطفا ابتدا نام را وارد کنید", "ویرایش کاربر");
+                return;
+            }
+            if (string.IsNullOrEmpty(_clientsViewModel.LastName))
+            {
+                MessageBox.Show("لطفا ابتدا فامیلی را وارد کنید", "ویرایش کاربر");
+                return;
+            }
+            client.FirstName = _clientsViewModel.FirstName;
+            client.LastName = _clientsViewModel.LastName;
+            client.Tier = _clientsViewModel.Tier;
+            await _clientsStore.EditClient(client);
+        }
+        #endregion
+    }
+}
