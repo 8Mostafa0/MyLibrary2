0a763c0122272451203d66f625154dff66664667
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:14:58 +0330

    add MainViewModel

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLibrary.ViewModel.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private readonly LayoutViewModel _layoutViewModel;
+        private ModalNavigationStore _modalNavigationStore;
+        public ViewModelBase CurrentViewModel => _layoutViewModel;
+        public ViewModelBase CurrentModalView => _modalNavigationStore.CurrentViewModel;
+
+        public bool IsModalOpen => _modalNavigationStore.IsModalOpen;
+        #endregion
+        #region Constructor
+        public MainViewModel(LayoutViewModel layoutViewModel)
+        {
+            _layoutViewModel = layoutViewModel;
+            _modalNavigationStore = new ModalNavigationStore();
+            _modalNavigationStore.CurrentViewModelChanged += OnModalChanged;
+            new LoginModalCommand(_modalNavigationStore, new LoanRepository(), new DbContextFactory(), new SettingsStore()).Execute(null);
+
+        }
+        #endregion
+        #region Methods
+
+        /// <summary>
+        /// get called each tim change value of modal navigation event trigred
+        /// </summary>
+        private void OnModalChanged()
+        {
+            OnProperychanged(nameof(CurrentModalView));
+            OnProperychanged(nameof(IsModalOpen));
+        }
+
+        #endregion
+    }
+}
