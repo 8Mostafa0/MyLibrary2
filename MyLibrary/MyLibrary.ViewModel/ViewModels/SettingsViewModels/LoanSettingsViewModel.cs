263d2bb69a2383745518ee0588b0b84760609959
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:20:01 +0330

    add LoanSettingsViewModel

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/SettingsViewModels/LoanSettingsViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+
+namespace MyLibrary.ViewModel.ViewModels.SettingsViewModels
+{
+    public class LoanSettingsViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private SettingsStore _settingsStore;
+        private int _maxBooksCount;
+        public string MaxBooksCount
+        {
+            get => _maxBooksCount.ToString();
+
+            set
+            {
+                if (!int.TryParse(value, out _maxBooksCount))
+                {
+                    MessageBox.Show("لطفا تعداد حداکثر کتاب را عدد وارد کنید", "خطای ورودی");
+                    return;
+                }
+                OnProperychanged(nameof(MaxBooksCount));
+                SettingsChanged();
+            }
+        }
+        private int _maxLoanDay;
+        public string MaxLoanDay
+        {
+            get => _maxLoanDay.ToString();
+
+            set
+            {
+                if (!int.TryParse(value, out _maxLoanDay))
+                {
+                    MessageBox.Show("لطفا تعداد حداکثر تعداد روز را عدد وارد کنید", "خطای ورودی");
+                    return;
+                }
+                OnProperychanged(nameof(MaxLoanDay));
+                SettingsChanged();
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public LoanSettingsViewModel()
+        {
+            _settingsStore = new SettingsStore();
+            Dictionary<string, int> setting = _settingsStore.GetLoansSetting();
+            MaxBooksCount = setting["MaxBooksLoan"].ToString();
+            MaxLoanDay = setting["MaxLoanDays"].ToString();
+        }
+
+        #endregion
+
+        #region Methods
+        public void SettingsChanged()
+        {
+            Dictionary<string, int> setting = _settingsStore.GetLoansSetting();
+            setting["MaxBooksLoan"] = int.Parse(MaxBooksCount);
+            setting["MaxLoanDays"] = int.Parse(MaxLoanDay);
+            _settingsStore.SaveLoanSettings(setting);
+        }
+        #endregion
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/SettingsViewModels/MainLayoutSettingsViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace MyLibrary.ViewModel.ViewModels.SettingsViewModels
+{
+    public class MainLayoutSettingViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private SettingsStore _settingStore;
+        private bool _booksCount;
+        public bool BooksCount
+        {
+            get => _booksCount;
+            set
+            {
+                _booksCount = value;
+                OnProperychanged(nameof(BooksCount));
+                OnDataChanged();
+            }
+        }
+        private bool _loansCount;
+        public bool LoansCount
+        {
+            get => _loansCount;
+            set
+            {
+                _loansCount = value;
+                OnProperychanged(nameof(LoansCount));
+                OnDataChanged();
+            }
+        }
+        private bool _dilayedLoansCount;
+        public bool DilayedLoansCount
+        {
+            get => _dilayedLoansCount;
+            set
+            {
+                _dilayedLoansCount = value;
+                OnProperychanged(nameof(DilayedLoansCount));
+                OnDataChanged();
+            }
+        }
+        private bool _clientsCount;
+        public bool ClientsCount
+        {
+            get => _clientsCount;
+            set
+            {
+                _clientsCount = value;
+                OnProperychanged(nameof(ClientsCount));
+                OnDataChanged();
+            }
+        }
+        #endregion
+
+        #region Contructor
+        public MainLayoutSettingViewModel()
+        {
+            _settingStore = new SettingsStore();
+            Dictionary<string, bool> settings = _settingStore.GetLayoutSettings();
+            ClientsCount = settings["ShowClientsCount"];
+            BooksCount = settings["ShowBooksCount"];
+            LoansCount = settings["ShowLoanedBooksCount"];
+            DilayedLoansCount = settings["ShowNotReturnedLoans"];
+        }
+
+        #endregion
+
+        #region Methods
+
+
+        private void OnDataChanged()
+        {
+            Dictionary<string, bool> settings = _settingStore.GetLayoutSettings();
+            settings["ShowClientsCount"] = ClientsCount;
+            settings["ShowBooksCount"] = BooksCount;
+            settings["ShowLoanedBooksCount"] = LoansCount;
+            settings["ShowNotReturnedLoans"] = DilayedLoansCount;
+            _settingStore.SaveLayoutSettings(settings);
+        }
+        #endregion
+    }
+}
