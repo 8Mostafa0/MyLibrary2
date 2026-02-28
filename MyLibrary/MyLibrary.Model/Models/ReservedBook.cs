ff937df7336b13883b6d1bbef7944cb19674ece3
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:11:25 +0330

    add ReservedBook Model

---
+++ MyLibrary/MyLibrary.Model/Models/ReservedBook.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyLibrary.Model.Models
+{
+    public class ReservedBook
+    {
+        public int ID { get; set; }
+        public int BookId { get; set; }
+        public int ClientId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
