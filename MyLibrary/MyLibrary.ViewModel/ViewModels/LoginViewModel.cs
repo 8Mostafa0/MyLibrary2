341326df3879fce9d95fd8c1c5b19b2a9350c18d
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:15:44 +0330

    add LoginViewModel

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Input;
+
+namespace MyLibrary.ViewModel.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        #region Dependencies
+        private string _password;
+        public bool FirstOpen { get; }
+        public string Title { get; set; }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnProperychanged(nameof(Password));
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand CloseAppCommand { get; }
+        public ICommand LoginCommand { get; }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel(ModalNavigationStore modalNavigationStore, LoanRepository loanRepository, DbContextFactory dbContextFactory, SettingsStore settingsStore)
+        {
+            CloseAppCommand = new CloseAppCommand();
+            LoginCommand = new LoginCommand(this, modalNavigationStore, loanRepository, dbContextFactory, settingsStore);
+            if (new SettingsStore().GetHashedPassword().IsNullOrEmpty())
+            {
+                FirstOpen = true;
+                Title = "رمزی برای پنل مشخص کنید";
+            }
+            else
+            {
+                Title = "رمز عبور خود را وارد کنید";
+            }
+        }
+        #endregion
+    }
+}
