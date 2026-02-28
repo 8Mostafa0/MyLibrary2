43eed9857d22dd144d6cd971a100f0ea50863b6b
8mostafa0 <mosielite@gmail.com>
2026-02-28 16:47:21 +0330

    add LoginViewModel with Interface

---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/Interfaces/ILoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLibrary.ViewModel.ViewModels.Interfaces
+{
+    public interface ILoginViewModel : IViewModelBase
+    {
+        ICloseAppCommand CloseAppCommand { get; }
+        bool FirstOpen { get; }
+        ILoginCommand LoginCommand { get; }
+        string Password { get; set; }
+        string Title { get; set; }
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MyLibrary.ViewModel.Stores.Interfaces;
+using MyLibrary.ViewModel.ViewModels.Interfaces;
+
+namespace MyLibrary.ViewModel.ViewModels
+{
+    public class LoginViewModel : ViewModelBase, ILoginViewModel
+    {
+        #region Dependencies
+        private string _password;
+        public bool FirstOpen { get; }
+        public string Title { get; set; }
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnProperychanged(nameof(Password));
+            }
+        }
+        #endregion
+
+        #region Commands
+        public ICloseAppCommand CloseAppCommand { get; }
+        public ILoginCommand LoginCommand { get; }
+
+        #endregion
+
+        #region Constructor
+        public LoginViewModel(IModalNavigationStore modalNavigationStore, ILoanRepository loanRepository, IDbContextFactory dbContextFactory, ISettingsStore settingsStore)
+        {
+            CloseAppCommand = new CloseAppCommand();
+            LoginCommand = new LoginCommand(this, modalNavigationStore, loanRepository, dbContextFactory, settingsStore);
+            if (settingsStore.GetHashedPassword().IsNullOrEmpty())
+            {
+                FirstOpen = true;
+                Title = "رمزی برای پنل مشخص کنید";
+            }
+            else
+            {
+                Title = "رمز عبور خود را وارد کنید";
+            }
+        }
+        #endregion
+    }
+}
