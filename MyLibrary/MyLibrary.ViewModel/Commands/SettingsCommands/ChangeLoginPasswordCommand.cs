4431fc331553941bb295bc89676daf8266b39557
8mostafa0 <mosielite@gmail.com>
2026-02-28 17:35:34 +0330

    add ChangeLoginPasswordCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/SettingsCommands/ChangeLoginPasswordCommand.cs
@@ -0,0 +1,51 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.SettingsViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary.ViewModel.Commands.SettingsCommands
+{
+    public class ChangeLoginPasswordCommand : CommandBase
+    {
+        #region Dependencies
+        private SettingsStore _settingsStore;
+        private SettingNavigationStore _settingNavigationStore;
+        private SecuritySettingsViewModel _securitySettingViewModel;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// save validated password to registry
+        /// </summary>
+        /// <param name="securitySettingsViewModel"></param>
+        /// <param name="settingNavigationStore"></param>
+        /// <param name="settingsStore"></param>
+        public ChangeLoginPasswordCommand(SecuritySettingsViewModel securitySettingsViewModel, SettingNavigationStore settingNavigationStore, SettingsStore settingsStore)
+        {
+            _settingsStore = settingsStore;
+            _settingNavigationStore = settingNavigationStore;
+            _securitySettingViewModel = securitySettingsViewModel;
+        }
+        #endregion
+
+        #region Execution
+        /// <summary>
+        /// </summary>
+        /// <param name="parameter">no marametes needed</param>
+        public override void Execute(object? parameter)
+        {
+            if (_securitySettingViewModel.Password.IsNullOrEmpty() || _securitySettingViewModel.Password.Count() < 5)
+            {
+                MessageBox.Show("لطفا رمز عبور را بیشتر از 5 حرف وارد کنید", "رمز عبور");
+                return;
+            }
+            _settingsStore.SaveNoneHashedPassword(_securitySettingViewModel.Password);
+            _settingNavigationStore.CurrentSettingViewModel = null;
+            MessageBox.Show("رمز عبور با موفقیت تغییر یافت", "رمز عبور");
+        }
+        #endregion
+    }
+}
