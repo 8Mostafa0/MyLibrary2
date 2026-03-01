6618c88b9368a1c7337051cec8ca8cb71e46a3e2
8mostafa0 <mosielite@gmail.com>
2026-03-01 08:53:30 +0330

    add LoggerService

---
+++ MyLibrary/MyLibrary.ViewModel/Servicies/LoggerService.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLibrary.ViewModel.Servicies
+{
+    /// <summary>
+    /// class for recording logs
+    /// </summary>
+    public static class LoggerService
+    {
+        private static ILogger _logger;
+        public static ILogger Logger
+        {
+            get
+            {
+                if (_logger is null)
+                {
+                    _logger = CreateLogger();
+                }
+                return _logger;
+            }
+
+        }
+        /// <summary>
+        /// Create instance of serilogger and Configure it.
+        /// </summary>
+        /// <returns></returns>
+        public static ILogger CreateLogger()
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+            return logger;
+
+        }
+
+    }
+}
