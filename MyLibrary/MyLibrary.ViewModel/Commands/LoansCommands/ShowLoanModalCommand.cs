b3da33cf853d158401681962c7b2221f19465f95
8mostafa0 <mosielite@gmail.com>
2026-03-01 12:38:35 +0330

    fix ShowLoanModalCommand

--- MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/ShowLoanModalCommand.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/ShowLoanModalCommand.cs
@@ -1,10 +1,7 @@
-﻿using MyLibrary.ViewModel.Stores;
+﻿using MyLibrary.Model.Models;
+using MyLibrary.Model.Repositories;
+using MyLibrary.ViewModel.Stores;
 using MyLibrary.ViewModel.ViewModels.LoanViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyLibrary.ViewModel.Commands.LoansCommands
 {
