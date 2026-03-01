f3deddf87ff8c4efe4fb6560a80023e09b5461a4
8mostafa0 <mosielite@gmail.com>
2026-03-01 12:35:15 +0330

    fix LoadReservedBookCommand

--- MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/LoadReservedBooksCommand.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/ReserveBoookCommands/LoadReservedBooksCommand.cs
@@ -1,9 +1,4 @@
 ﻿using MyLibrary.ViewModel.Stores;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
 {
@@ -31,7 +26,7 @@ namespace MyLibrary.ViewModel.Commands.ReserveBoookCommands
         /// <summary>
         /// </summary>
         /// <param name="parameter">no marametes needed</param>
-        public override async void Execute(object? parameter)
+        public override async void Execute(object parameter)
         {
             await _reservedBooksStore.Load();
         }
