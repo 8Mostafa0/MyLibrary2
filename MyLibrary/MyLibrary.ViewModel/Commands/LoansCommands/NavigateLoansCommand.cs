15e618ca9f027a42c0454cd820642e0cd6b8047b
8mostafa0 <mosielite@gmail.com>
2026-03-01 12:43:19 +0330

    fix NavigateLoansCommand

--- MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/NavigateLoansCommand.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/NavigateLoansCommand.cs
@@ -1,4 +1,5 @@
-﻿using MyLibrary.ViewModel.Stores;
+﻿using MyLibrary.Model.Repositories;
+using MyLibrary.ViewModel.Stores;
 using MyLibrary.ViewModel.ViewModels.LoanViewModels;
 
 namespace MyLibrary.ViewModel.Commands.LoansCommands
@@ -36,7 +37,7 @@ namespace MyLibrary.ViewModel.Commands.LoansCommands
         /// <summary>
         /// </summary>
         /// <param name="parameter">no marametes needed</param>
-        public override void Execute(object? parameter)
+        public override void Execute(object parameter)
         {
             _navigationStore.ContentScreen = _loansViewModel;
         }
