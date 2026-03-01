0d5bafcdeb25eff51a985b9e631fc9c1ce197e38
8mostafa0 <mosielite@gmail.com>
2026-03-01 08:52:48 +0330

    add NavigationService With Interface

---
+++ MyLibrary/MyLibrary.ViewModel/Servicies/INavigationService.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLibrary.ViewModel.Servicies
+{
+    public interface INavigationService
+    {
+        void NavigateContentViewModel();
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/Servicies/NavigationService.cs
@@ -0,0 +1,40 @@
+﻿using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels;
+using System;
+
+namespace MyLibrary.ViewModel.Servicies
+{
+    public class NavigationService : INavigationService
+    {
+        #region Dependencies
+        private readonly Func<ViewModelBase> _createMainViewModel;
+
+        public NavigationStore NavigationStore;
+        #endregion
+
+        #region Contructor
+        /// <summary>
+        /// set value of Func view to main view of main navigation view
+        /// </summary>
+        /// <param name="navigationStore"></param>
+        /// <param name="createMainViewModel"></param>
+        public NavigationService(NavigationStore navigationStore, Func<ViewModelBase> createMainViewModel)
+        {
+            NavigationStore = navigationStore;
+            _createMainViewModel = createMainViewModel;
+        }
+        #endregion
+
+        #region Methods
+        public void NavigateMainVewModel()
+        {
+            NavigationStore.MainContentViewModel = _createMainViewModel();
+        }
+
+        public void NavigateContentViewModel()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
