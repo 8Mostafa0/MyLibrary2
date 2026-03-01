d44ebd6238bf375ce2e348d0d277c0b4d6f9cc55
8mostafa0 <mosielite@gmail.com>
2026-03-01 12:40:38 +0330

    fix ShowEditLoanViewModel

--- MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/ShowEditLoanViewModel.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/LoansCommands/ShowEditLoanViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace MyLibrary.ViewModel.Commands.LoansCommands
+﻿using MyLibrary.Model.Repositories;
+using MyLibrary.ViewModel.Stores;
+using MyLibrary.ViewModel.ViewModels.LoanViewModels;
+
+namespace MyLibrary.ViewModel.Commands.LoansCommands
 {
     public class ShowEditLoanViewModel : CommandBase
     {
@@ -48,13 +52,13 @@
         /// <summary>
         /// </summary>
         /// <param name="parameter">no marametes needed</param>
-        public override async void Execute(object? parameter)
+        public override async void Execute(object parameter)
         {
             if (_loansViewModel.SelectedLoan is null)
             {
                 MessageBox.Show("لطفا ابتدا امانتی را انتخاب کنید", "ویرایش نوبت");
             }
-            else if (_loansViewModel.SelectedLoan.ReturnedDate is not null && _loansViewModel.SelectedLoan.ReturnedDate != "خیر")
+            else if (!(_loansViewModel.SelectedLoan.ReturnedDate is null) && _loansViewModel.SelectedLoan.ReturnedDate != "خیر")
             {
                 MessageBox.Show("این امانت تحویل داده شده است", "ویرایش نوبت");
             }
