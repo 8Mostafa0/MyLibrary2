51104ae6ca7fd7beb57a3d35f790641cf0641484
8mostafa0 <mosielite@gmail.com>
2026-03-02 16:17:15 +0330

    create AddNewClientCommand Interface

--- MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/AddNewClientCommand.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/AddNewClientCommand.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace MyLibrary.ViewModel.Commands.ClientsCommands
 {
-    public class AddNewClientCommand : CommandBase
+    public class AddNewClientCommand : CommandBase, IAddNewClientCommand
     {
         #region Dependencies
         private readonly IClientsStore _clientStore;
---
+++ MyLibrary/MyLibrary.ViewModel/Commands/ClientsCommands/IAddNewClientCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLibrary.ViewModel.Commands.ClientsCommands
+{
+    public interface IAddNewClientCommand
+    {
+        void Execute(object parameter);
+    }
+}
