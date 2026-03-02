4fc077975c94c2d590572938c7f588bc38d82804
8mostafa0 <mosielite@gmail.com>
2026-03-02 16:41:52 +0330

    create ReservedBooksRepository Interface

---
+++ MyLibrary/MyLibrary.Model/Repositories/IReservedBooksRepository.cs
@@ -0,0 +1,20 @@
+﻿using MyLibrary.Model.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Model.Repositories
+{
+    public interface IReservedBooksRepository
+    {
+        Task AddNewReservedBookToDb(ReservedBook reservedBook);
+        Task<ReservedBook> BookAlreadyRegistred(int bookId);
+        Task DeleteReservedBookToDb(int bookId);
+        Task DeleteReservedBookWithClientToDb(ReservedBook reservedBook);
+        Task EditReservBookToDb(ReservedBook reservedBook);
+        Task<List<ReservedBook>> GetAllReservedBooks(string customSql = "");
+        Task<List<ReservedBook>> GetReservationForBook(int bookId);
+        Task<ReservedBook> GetReservedBook(string customSql, string executionPart);
+        Task RemoveClientReservedBooks(int clientId);
+        Task<ReservedBook> UserHaveReservedBook(int clientId);
+    }
+}
--- MyLibrary/MyLibrary.Model/Repositories/ReservedBooksRepository.cs
+++ MyLibrary/MyLibrary.Model/Repositories/ReservedBooksRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MyLibrary.Model.Repositories
 {
-    public class ReservedBooksRepository
+    public class ReservedBooksRepository : IReservedBooksRepository
     {
         #region Dependencies
         private DbContextFactory _dbContextFactory;
