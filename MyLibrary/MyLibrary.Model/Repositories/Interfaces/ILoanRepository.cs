a6fbd3aed066db6c92bdcd7b21819f126abd9d5e
8mostafa0 <mosielite@gmail.com>
2026-03-02 16:43:52 +0330

    create LoanRepository Interfaces

---
+++ MyLibrary/MyLibrary.Model/Repositories/Interfaces/ILoanRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyLibrary.Model.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Model.Repositories
+{
+    public interface ILoanRepository
+    {
+        Task AddNewLoanToDb(Loan loan);
+        Task DeleteLoanInDB(Loan loan);
+        Task<List<Loan>> GetAllClientLoans(int clientId);
+        Task<List<Loan>> GetAllLoans(string customSql = "");
+        Task<Loan> GetLoan(string customSql, string executionPart);
+        Task<List<Loan>> GetNotReturnedLoanOfBook(int bookId);
+        Task RemoveBookLoans(int bookId);
+        Task RemoveClientLoans(int id);
+        Task SetLoanReturned(Loan loan);
+        Task UpdateLoanAtDb(Loan loan);
+        Task<List<Loan>> UserHaveDilayedLoan(int clientId);
+    }
+}
--- MyLibrary/MyLibrary.Model/Repositories/LoanRepository.cs
+++ MyLibrary/MyLibrary.Model/Repositories/LoanRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MyLibrary.Model.Repositories
 {
-    public class LoanRepository
+    public class LoanRepository : ILoanRepository
     {
         #region Contructor
         private readonly DbContextFactory _dbContextFactory;
