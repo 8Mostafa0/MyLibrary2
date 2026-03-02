a9aea43abbf676c835901de7acfb7381fc0b0464
8mostafa0 <mosielite@gmail.com>
2026-03-02 15:34:28 +0330

    create ModalNavigationStore Interface

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/IModalNavigationStore.cs
@@ -0,0 +1,15 @@
+﻿using MyLibrary.ViewModel.ViewModels;
+using System;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public interface IModalNavigationStore
+    {
+        IViewModelBase CurrentViewModel { get; set; }
+        bool IsModalOpen { get; }
+
+        event Action CurrentViewModelChanged;
+
+        void Close();
+    }
+}
--- MyLibrary/MyLibrary.ViewModel/Stores/ModalNavigationStore.cs
+++ MyLibrary/MyLibrary.ViewModel/Stores/ModalNavigationStore.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace MyLibrary.ViewModel.Stores
 {
-    public class ModalNavigationStore
+    public class ModalNavigationStore : IModalNavigationStore
     {
         #region Dependencies
         private IViewModelBase _currentViewModel;
