284dba1df337546ac09a16b45bbd18e9afc03650
8mostafa0 <mosielite@gmail.com>
2026-03-02 16:21:09 +0330

    create AddNewBookCommand Interface

--- MyLibrary/MyLibrary.ViewModel/Commands/BooksCommands/AddNewBookCommand.cs
+++ MyLibrary/MyLibrary.ViewModel/Commands/BooksCommands/AddNewBookCommand.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace MyLibrary.ViewModel.Commands.BooksCommands
 {
-    public class AddNewBookCommand : CommandBase
+    public class AddNewBookCommand : CommandBase, IAddNewBookCommand
     {
         #region Dependencies
         private IBooksStore _booksStore;
---
+++ MyLibrary/MyLibrary.ViewModel/Commands/Interfaces/IBooksCommands/IAddNewBookCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace MyLibrary.ViewModel.Commands.BooksCommands
+{
+    public interface IAddNewBookCommand
+    {
+        void Execute(object parameter);
+    }
+}
