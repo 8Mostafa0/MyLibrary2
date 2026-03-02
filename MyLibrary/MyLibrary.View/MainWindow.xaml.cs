8b81ba115f2cb6c8ace4b598884d14ddb356663b
8mostafa0 <mosielite@gmail.com>
2026-03-02 08:28:44 +0330

    map views to viewmodels

--- MyLibrary/MyLibrary.View/MainWindow.xaml.cs
+++ MyLibrary/MyLibrary.View/MainWindow.xaml.cs
@@ -11,13 +11,5 @@ namespace MyLibrary.View
         {
             InitializeComponent();
         }
-        private void OnShowModalClick(object sender, RoutedEventArgs e)
-        {
-            modal.IsOpen = true;
-        }
-        private void OnCloseModalClick(object sender, RoutedEventArgs e)
-        {
-            modal.IsOpen = false;
-        }
     }
 }
