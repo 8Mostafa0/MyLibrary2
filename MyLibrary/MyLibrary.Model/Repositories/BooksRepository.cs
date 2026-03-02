9eac0f7c9634b15201700a0b078d935f70b5be01
8mostafa0 <mosielite@gmail.com>
2026-03-02 16:44:32 +0330

    create BooksRepository Interfaces

--- MyLibrary/MyLibrary.Model/Repositories/BooksRepository.cs
+++ MyLibrary/MyLibrary.Model/Repositories/BooksRepository.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace MyLibrary.Model.Repositories
 {
-    public class BooksRepository
+    public class BooksRepository : IBooksRepository
     {
         #region Dependencies
         private DbContextFactory _dbContextFactory;
---
+++ MyLibrary/MyLibrary.Model/Repositories/IBooksRepository.cs
@@ -0,0 +1,16 @@
+﻿using MyLibrary.Model.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyLibrary.Model.Repositories
+{
+    public interface IBooksRepository
+    {
+        Task AddNewBookToDb(Book book);
+        Task DeleteBookInDb(Book book);
+        Task EditeBookInDb(Book book);
+        Task<List<Book>> GetAllBooks(string customSql = "");
+        Task<Book> GetBookById(int id, string customSql);
+        Task<List<Book>> GetBooksByName(string bookName);
+    }
+}
