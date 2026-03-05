3d7d3a98aabe74dcbeaf0f00986ac1926d7a1c22
8mostafa0 <mosielite@gmail.com>
2026-03-05 11:31:00 +0330

    create Service for requests to BookApi

---
+++ MyLibrary/MyLibrary.ViewModel/Services/BookApi/BookApi.cs
@@ -0,0 +1,45 @@
+﻿using MyLibrary.Model.Models;
+using MyLibrary.ViewModel.Stores;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+namespace MyLibrary.ViewModel.Services.BookApi
+{
+    public class BookApi
+    {
+        private IBookApiStore _bookApiStore;
+        public BookApi(IBookApiStore bookApiStore)
+        {
+            _bookApiStore = bookApiStore;
+        }
+
+        public async void GetBookInfo(int bookNumber)
+        {
+            const string apiUrl = "https://gutendex.com/books/";
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync($"{apiUrl}{bookNumber}");
+                    response.EnsureSuccessStatusCode();
+                    await response.Content.ReadAsStringAsync().ContinueWith(t =>
+                    {
+                        JObject data = JObject.Parse(t.Result);
+                        NewBook book = new NewBook
+                        {
+                            ID = (int)data["id"],
+                            Name = (string)data["title"],
+                            Publisher = string.Join(", ", data["authors"][0]),
+                            Desciption = string.Join(", ", data["summaries"]["e"])
+                        };
+                        _bookApiStore.BookData = book;
+                    });
+                }
+                catch (HttpRequestException e)
+                {
+                    _bookApiStore.BookData = new NewBook()
+                    { ID = 0, Name = "Error", Desciption = e.ToString() };
+                }
+            }
+        }
+    }
+}
