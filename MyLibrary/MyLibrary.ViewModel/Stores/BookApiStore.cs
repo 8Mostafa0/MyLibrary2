93dbfb516800341b8913c39931ea7baaa17c8444
8mostafa0 <mosielite@gmail.com>
2026-03-05 11:02:28 +0330

    create BookApiStore and Interface

---
+++ MyLibrary/MyLibrary.ViewModel/Stores/BookApiStore.cs
@@ -0,0 +1,33 @@
+﻿using MyLibrary.Model.Models;
+using System;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public class BookApiStore : IBookApiStore
+    {
+        #region Dependencies
+        public event Action BookDataChanged;
+
+        private Book _bookData;
+        public Book BookData
+        {
+            get => _bookData;
+            set
+            {
+                _bookData = value;
+                OnBookDataChange();
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Triger event of when data is changed to update the view
+        /// </summary>
+        private void OnBookDataChange()
+        {
+            BookDataChanged?.Invoke();
+        }
+        #endregion
+    }
+}
---
+++ MyLibrary/MyLibrary.ViewModel/Stores/IBookApiStore.cs
@@ -0,0 +1,12 @@
+﻿using MyLibrary.Model.Models;
+using System;
+
+namespace MyLibrary.ViewModel.Stores
+{
+    public interface IBookApiStore
+    {
+        Book BookData { get; set; }
+
+        event Action BookDataChanged;
+    }
+}
