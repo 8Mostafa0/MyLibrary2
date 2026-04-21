0f42771ccea6f66a50a5c8f348c822f40d744ef9
8mostafa0 <mosielite@gmail.com>
2026-04-21 08:49:24 +0330

    add MyLibraryDbContext

---
+++ MyLibrary/MyLibrary.Model/Repositories/Interfaces/IMyLibraryDbContext.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibrary.Model.Repositories
+{
+    public interface IMyLibraryDbContext
+    {
+        IBooksRepository BooksRepository { get; set; }
+        IClientsRepository ClientsRepository { get; set; }
+        ILoanRepository LoanRepository { get; set; }
+        IReservedBooksRepository ReservedBooksRepository { get; set; }
+
+        void Dispose();
+    }
+}
---
+++ MyLibrary/MyLibrary.Model/Repositories/MyLibraryDbContext.cs
@@ -0,0 +1,24 @@
+﻿namespace MyLibrary.Model.Repositories
+{
+    public class MyLibraryDbContext : IMyLibraryDbContext
+    {
+        public IClientsRepository ClientsRepository { get; set; }
+        public IBooksRepository BooksRepository { get; set; }
+        public IReservedBooksRepository ReservedBooksRepository { get; set; }
+        public ILoanRepository LoanRepository { get; set; }
+        public MyLibraryDbContext(IClientsRepository clientsRepository, IBooksRepository booksRepository, IReservedBooksRepository reservedBooksRepository, ILoanRepository loanRepository)
+        {
+            ClientsRepository = clientsRepository;
+            BooksRepository = booksRepository;
+            ReservedBooksRepository = reservedBooksRepository;
+            LoanRepository = loanRepository;
+        }
+
+
+        public void Dispose()
+        {
+            BooksRepository?.Dispose();
+
+        }
+    }
+}
