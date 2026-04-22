1de58a08e3ebd5d917affd4eca4f8140c2abf194
8mostafa0 <mosielite@gmail.com>
2026-04-22 10:01:25 +0330

    add RelayCommand and AsyncRelayCommand

---
+++ MyLibrary/MyLibrary.ViewModel/Commands/BaseCommands/AsyncRelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyLibrary.ViewModel.Commands.BaseCommands
+{
+    public class AsyncRelayCommand<T> : ICommand
+    {
+        private readonly Func<T, Task> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        public AsyncRelayCommand(Func<T, Task> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        public async void Execute(object parameter)
+        {
+            await _execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+    }
+}
