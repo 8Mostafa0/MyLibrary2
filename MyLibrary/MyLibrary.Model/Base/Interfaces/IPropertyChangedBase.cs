6144533fa35e2d59e11221ec7780d2754d921f5b
8mostafa0 <mosielite@gmail.com>
2026-04-26 15:22:03 +0330

    add PropertyChangedBase and interface

---
+++ MyLibrary/MyLibrary.Model/Base/Interfaces/IPropertyChangedBase.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace MyLibrary.Model.Base
+{
+    public interface IPropertyChangedBase
+    {
+        event PropertyChangedEventHandler PropertyChanged;
+
+        void Dispose();
+    }
+}
---
+++ MyLibrary/MyLibrary.Model/Base/PropertyChangedBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MyLibrary.Model.Base
+{
+    public class PropertyChangedBase : IPropertyChangedBase
+    {
+        #region EVENTS
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        #region METHODS
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void SetProperty<T>(ref T field, T value, Action eventAction)
+        {
+            if (!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                eventAction?.Invoke();
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Implementation
+
+        public virtual void Dispose() { }
+
+        #endregion
+    }
+}
